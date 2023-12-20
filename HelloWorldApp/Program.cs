73a5eb3d5c5d7095c7bc493966b4de468f10a7e8
matuszeq <matuszek@amorki.pl>
2023-12-20 19:38:49 +0000

    s

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorldApp
+{
+    class MyClass
+    {
+        public static void Main()
+        {
+            // Stworzenie pudelka
+            string imie;
+
+
+            Console.Write("Hej, Jak masz na imie: ");
+            imie = Console.ReadLine();
+            
+            Console.Write(imie);
+
+
+        }
+    }
+}
